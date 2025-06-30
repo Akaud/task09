edc33478c9c45896b23ec1985d015d462e599a3a
Akaud <akadlevchenko@gmail.com>
2025-06-30 15:45:26 +0300

    try_02

--- task09/modules/afw/main.tf
+++ task09/modules/afw/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_firewall" "afw" {
   name                = local.firewall_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku_name            = "Standard"
+  sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
 
   ip_configuration {
